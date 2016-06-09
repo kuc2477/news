2fd1c7f582ee40c8fed5d659b146a88cd9d2c674
Ha Junsoo <kuc2477@gmail.com>
2016-06-09 16:54:18 +0900

    cleanup tests

--- tests/test_cover.py
+++ tests/test_cover.py
@@ -1,44 +1,24 @@
-import functools
 import pytest
 from news.reporters.url import URLReporter
 from news.contrib.logging.middlewares import (
-    logging_dispatch_middleware,
-    logging_fetch_middleware,
+    request_log_middleware,
+    response_log_middleware,
+    report_log_middleware,
 )
 
 
 @pytest.mark.asyncio
 async def test_cover_prepare(cover, django_root_news):
-    # dispatch middleware
-    def dispatch_middleware(reporter, dispatch):
-        @functools.wraps(dispatch)
-        async def enhanced():
-            return [1, 2, 3]
-        return enhanced
-
-    # fetch middleware
-    def fetch_middleware(reporter, fetch):
-        @functools.wraps(fetch)
-        async def enhanced():
-            return 10
-        return enhanced
-
     assert(cover.reporter is None)
     cover.prepare(
         reporter_class=URLReporter,
-        dispatch_middlewares=[
-            logging_fetch_middleware,
-            dispatch_middleware
-        ],
-        fetch_middlewares=[
-            logging_dispatch_middleware,
-            fetch_middleware
-        ]
+        request_middlewares=[request_log_middleware],
+        response_middlewares=[response_log_middleware],
+        report_middlewares=[report_log_middleware],
     )
 
     assert(cover.reporter is not None)
     assert(cover.reporter.backend == cover.backend)
-
-    # check middlewares has been applied properly
-    assert(await cover.reporter.dispatch() == [1, 2, 3])
-    assert(await cover.reporter.fetch() == 10)
+    assert(cover.request_middlewares)
+    assert(cover.response_middlewares)
+    assert(cover.report_middlewares)
